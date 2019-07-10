066b9f50fc83798bb41b3478ada04c5135553b9c
o.tsivun <alexandr.ts@gmail.com>
2019-07-10 22:34:01 +0300

    migration for create some user fields and git ignore

---
+++ database/migrations/2019_07_10_192103_add_fields_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('city')->after('password')->default('');
+            $table->string('phone')->after('city')->default('');
+            $table->string('icon')->after('phone')->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('city');
+            $table->dropColumn('phone');
+            $table->dropColumn('icon');
+        });
+    }
+}
